1a62b63bcab41bce02e2779925eff485b9ff423e
Pablo Brodt <pablobrodt@gmail.com>
2016-10-14 20:34:08 -0300

    sp1: estrutura : ajustar controller de projetos

---
+++ controllers/ProjectController.js
@@ -0,0 +1,13 @@
+var models = require('../models');
+//var jsonUtils = require('../utils/jsonUtils');
+var q = require('q');
+var Project = models.Project;
+
+module.exports = function(app) {
+
+  //var cryptService = app.services.cryptService;
+
+  var ProjectController = {};
+
+  return UserController;
+}
