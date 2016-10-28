bb6a47d92c3287239a67807423dd8e65a65230f4
Mateus Schmitz <matteuschmitz@gmail.com>
2016-10-28 20:40:19 -0200

    Model Segment adicionado e OK

--- models/Segment.js
+++ models/Segment.js
@@ -1,13 +1,17 @@
 "use strict";
 
 module.exports = function(sequelize, DataTypes) {
-  var Project = sequelize.define("Project", {
-    id_project : {
+  var Segment = sequelize.define("Segment", {
+    id_segment : {
       type: DataTypes.INTEGER(11).UNSIGNED,
       primaryKey: true,
       autoIncrement: true
+    },
+    description : {
+        type : DataTypes.STRING(70),
+        allowNull: false
     }
   });
 
-  return Project;
+  return Segment;
 };
