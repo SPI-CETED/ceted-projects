e60b4a3dc8257ec7cca596ba45bc691c50f9a561
Luis Gustavo Bier Schuler <luis.schuler@gmail.com>
2016-11-18 20:41:29 -0200

    Fixing segments

--- routes/Segment.js
+++ routes/Segment.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
   var segmentController = app.controllers.SegmentController;
 
-  app.post('/v1/segment', segmentController.create);
+  app.post('/v1/segments', segmentController.insert);
 }
