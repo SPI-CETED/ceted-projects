37c7a282dd86b8b5e5af8dcb11289006382e1929
pablo <pablobrrodt@gmailcom>
2016-11-18 20:56:06 -0200

    Liberar rota

--- routes/ProjectRegistration.js
+++ routes/ProjectRegistration.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
   var controller = app.controllers.ProjectRegistrationController;
 
-  // app.get('/v1/projectsregistration', controller);
+  app.get('/v1/projectsregistration', controller.list);
   // app.get('/v1/projectsregistration/:id', controller);
   app.post('/v1/projectsregistration', controller.insert);
   app.put('/v1/projectsregistration/:id', controller.update);
