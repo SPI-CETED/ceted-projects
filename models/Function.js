8cd4cbb72fbd88d4d7b4176816381c9a83fed8f8
slave00 <slave00@slave00-debian.slave00>
2016-11-25 20:50:09 -0200

    Bugfix https://trello.com/c/pakroqG0/62-bug-funcoes

--- models/Function.js
+++ models/Function.js
@@ -1,13 +1,13 @@
 "use strict";
 
 module.exports = function(sequelize, DataTypes) {
-    var Function = sequelize.define("Functions", {
+    var Function = sequelize.define("Function", {
         id_function : {
             type: DataTypes.INTEGER(11).UNSIGNED,
             primaryKey: true,
             autoIncrement: true
         },
-        descricao: {
+        description: {
             type: DataTypes.STRING(50),
             allowNull: false
         }
