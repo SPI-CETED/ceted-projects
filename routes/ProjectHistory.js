261e42b362e07908e24f1c7c60d4406429463341
diegoludwig <diepcc@gmail.com>
2016-12-02 20:34:50 -0200

    Adicionar findById

--- routes/ProjectHistory.js
+++ routes/ProjectHistory.js
@@ -2,7 +2,7 @@ module.exports = function(app){
   var controller = app.controllers.ProjectHistoryController;
 
   // app.get('/v1/projects/history/', controller);
-  // app.get('/v1/projects/history/:id', controller)
+  app.get('/v1/projects/history/:id', controller.findById)
   app.post('/v1/projects/history/', controller.insert);
   // app.put('/v1/projects/history/:id', controller);
   // app.delete('/v1/projects/history/:id', controller);
