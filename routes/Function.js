745096f887d8499159b94d9897beb67d934563ea
diegoludwig <diepcc@gmail.com>
2016-12-02 21:17:43 -0200

    remover rota duplicada

--- routes/Function.js
+++ routes/Function.js
@@ -5,6 +5,5 @@ module.exports = function(app){
 	app.get('/v1/functions', functionController.list);
 	app.get('/v1/functions/:id', functionController.findById);
 	app.put('/v1/functions/:id', functionController.update);
-	app.get('/v1/functions/:id', functionController.findById);
     app.delete('/v1/functions/:id', functionController.delete);
 }
